fa813a913a15dfa680ce402e2107627e60d8c876
Ryan Britton <ryan@brittonmobile.com>
2016-09-17 14:49:01 -0700

    Added build target and scheme for Carthage framework.

---
+++ BoxContentSDK/BoxContentSDK iOS/BoxContentSDK iOS.h
@@ -0,0 +1,19 @@
+//
+//  BoxContentSDK iOS.h
+//  BoxContentSDK iOS
+//
+//  Created by Ryan on 9/17/16.
+//  Copyright © 2016 Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoxContentSDK iOS.
+FOUNDATION_EXPORT double BoxContentSDK_iOSVersionNumber;
+
+//! Project version string for BoxContentSDK iOS.
+FOUNDATION_EXPORT const unsigned char BoxContentSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoxContentSDK_iOS/PublicHeader.h>
+
+
